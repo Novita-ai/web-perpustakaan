a698ee4c3aaf8aa8a338dc891e98d593d4c3aac0
Novita-ai <wwwnovita17@gmail.com>
2022-06-08 22:36:07 +0800

    first commit

---
+++ perpus.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 21 Bulan Mei 2022 pada 09.45
+-- Versi server: 10.1.40-MariaDB
+-- Versi PHP: 7.1.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_anggota`
+--
+
+CREATE TABLE `tb_anggota` (
+  `id_anggota` int(11) NOT NULL,
+  `nim` int(11) NOT NULL,
+  `nama_anggota` varchar(50) NOT NULL,
+  `tempat_lahir` varchar(100) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `jk` enum('L','P') NOT NULL,
+  `prodi` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_anggota`
+--
+
+INSERT INTO `tb_anggota` (`id_anggota`, `nim`, `nama_anggota`, `tempat_lahir`, `tgl_lahir`, `jk`, `prodi`) VALUES
+(2, 323432111, 'watiq', 'bandung', '1998-01-01', 'P', 'Teknik Management'),
+(3, 3432123, 'Rudi Tabuti', 'Palembang', '1998-07-02', 'L', 'Sistem Operasi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_buku`
+--
+
+CREATE TABLE `tb_buku` (
+  `id_buku` int(11) NOT NULL,
+  `judul_buku` varchar(200) NOT NULL,
+  `pengarang_buku` varchar(100) NOT NULL,
+  `penerbit_buku` varchar(150) NOT NULL,
+  `tahun_terbit` varchar(4) NOT NULL,
+  `isbn` varchar(25) NOT NULL,
+  `jumlah_buku` int(3) NOT NULL,
+  `lokasi` enum('Rak 1','Rak 2','Rak 3') NOT NULL,
+  `tgl_input` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_buku`
+--
+
+INSERT INTO `tb_buku` (`id_buku`, `judul_buku`, `pengarang_buku`, `penerbit_buku`, `tahun_terbit`, `isbn`, `jumlah_buku`, `lokasi`, `tgl_input`) VALUES
+(4, 'Belajar HTML', 'surya', 'Erlangga', '2018', '234312', 3, 'Rak 3', '2020-07-21'),
+(6, 'Belajar Codeigniter', 'ridho', 'budi store', '2019', '45234521', 9, 'Rak 2', '2020-07-27');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `nim_transaksi` int(11) NOT NULL,
+  `id_anggota` int(11) NOT NULL,
+  `tgl_pinjam` varchar(50) NOT NULL,
+  `tgl_kembali` varchar(50) NOT NULL,
+  `status` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_transaksi`
+--
+
+INSERT INTO `tb_transaksi` (`id_transaksi`, `id_buku`, `nim_transaksi`, `id_anggota`, `tgl_pinjam`, `tgl_kembali`, `status`) VALUES
+(7, 4, 2, 2, '01-07-2020', '23-07-2020', 'kembali'),
+(8, 6, 3, 3, '01-07-2020', '5-07-2020', 'pinjam'),
+(11, 4, 3, 3, '13-07-2020', '20-07-2020', 'pinjam'),
+(12, 6, 3, 3, '21-05-2022', '28-05-2022', 'pinjam');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `username`, `password`, `nama`, `foto`) VALUES
+(4, 'rizqi', '$2y$10$pyk4IbyIAuE5Q8J2yV204.XZmMsMVQ7T.bdG1fSxxkBta9AvVZjSS', 'muhammad fathur', '6288977a50ab1.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_anggota`
+--
+ALTER TABLE `tb_anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- Indeks untuk tabel `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indeks untuk tabel `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indeks untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_anggota`
+--
+ALTER TABLE `tb_anggota`
+  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
